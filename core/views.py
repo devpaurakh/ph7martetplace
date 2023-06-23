07a815ba62b1b5b60340b011fe36340c4fdcaac6
Paurakh1 <paurakhsaud2022@gmail.com>
2023-06-23 20:34:36 +0545

    work done in detail page

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from item.models import Catagory, Item
 
 def index(request):
-    items = Item.objects.filter(is_sold= False)[0:6]
+    items = Item.objects.filter(is_sold= False)
     catagories = Catagory.objects.all()
     
     return render(request,'core/index.html',{
