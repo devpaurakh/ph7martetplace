761e8b62349026388de2a5c6ff1d2ffdc6698076
Paurakh1 <paurakhsaud2022@gmail.com>
2023-07-18 16:51:39 +0545

    signup is done and working properly

--- core/forms.py
+++ core/forms.py
@@ -6,5 +6,27 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username','email','password1','password2',)
+        
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your Username',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
+    
+    
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your Email address',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
+    
+    
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
+    
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
 
         
