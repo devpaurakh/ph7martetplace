3c9ef4fa8db1eab4b15724b58634f031c7c7f8cf
Paurakh1 <paurakhsaud2022@gmail.com>
2023-07-18 17:19:59 +0545

    loginDone

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+from .forms import LoginForm
 
 from . import views
 
@@ -8,4 +10,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html',authentication_form=LoginForm), name='login')
 ]
